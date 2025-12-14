bbbecc83ed61fbaadc497cdb8669535be6f999b5
danpub1 <danpub1@redoaktree.net>
2025-12-14 07:46:39 -0600

    Embed wasm in html

--- do.sh
+++ do.sh
@@ -2,3 +2,6 @@ GOOS=js GOARCH=wasm go build -o findanagrams.wasm
 gzip -9 -f -n findanagrams.wasm
 go build -tags web -o findanagramsweb
 go build
+base64 findanagrams.wasm.gz | gawk '{print $0 "\\"}' >findanagrams.wasm.gz.b64
+gawk -f embed.awk findanagrams.html.src >findanagrams.html
+rm findanagrams.wasm.gz.b64
